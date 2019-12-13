a9b9d12e271e1312e79dd3038ad30e1553312081
Ugudei <ugudeit@gmail.com>
2019-12-12 16:38:32 -0800

    Add files via upload

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,10 +1,38 @@
+
+var size = 0;
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  
-
+  var storage = {};
+  var someStorage = {}
+  _.extend(storage, stackMethods)
+  return storage;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+
+    this[size] = value;
+    size++
+
+  },
+  pop: function(){
+    if (size < 0) {
+      return 0;
+    }
+    size--;
+    var deleted = this[size];
+    delete this[size];
+    return deleted;
+  },
+  size: function(){
+
+    console.log(size);
+    return size;
+  }
+};
+
+
+
 
 
